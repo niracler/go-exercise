d523c447d34482cdc87dc946ac0906189bd5a47a
Niracler <1026037967@qq.com>
2019-11-15 11:31:35 +0800

    method

--- tree/node.go
+++ tree/node.go
@@ -1,10 +1,54 @@
 package main
 
+import "fmt"
+
 type treeNode struct {
-	value int
+	value       int
 	left, right *treeNode
 }
 
-func main() {
+func (node treeNode) printNode() {
+	fmt.Print(node.value, " ")
+}
 
+// 不加星号就是值传递了
+func (node *treeNode) setValue(value int) {
+	node.value = value
+}
+
+func createNode(value int) *treeNode {
+	return &treeNode{value: value}
+}
+
+//中序遍历
+func (node *treeNode) traverse() {
+	if node == nil {
+		return
+	}
+
+	node.left.traverse()
+	node.printNode()
+	node.right.traverse()
+}
+
+func main() {
+	var root treeNode
+
+	root = treeNode{value: 3}
+	root.left = &treeNode{}
+	root.right = &treeNode{5, nil, nil}
+
+	nodes := []treeNode{
+		{value: 3},
+		{},
+		{6, nil, &root},
+	}
+
+	fmt.Println(nodes)
+
+	root.right.left = createNode(646)
+	root.right.left.setValue(555)
+	root.right.left.printNode()
+	fmt.Println()
+	root.traverse()
 }
