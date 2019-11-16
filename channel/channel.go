9b52b30f452c326c5611b51fda8f433a51ae14e3
Niracler <1026037967@qq.com>
2019-11-16 16:38:32 +0800

    begin channels

--- channel/channel.go
+++ channel/channel.go
@@ -6,18 +6,33 @@ import (
 )
 
 func worker(id int, c chan int) {
-	for {
-		fmt.Printf("Worker %d received %c\n", id, <-c)
+	for n := range c {
+		fmt.Printf("Worker %d received %c\n", id, n)
 	}
 }
 
+func createWorker(id int) chan<- int {
+	c := make(chan int)
+	go worker(id, c)
+
+	return c
+}
+
+func bufferedChannel() {
+	c := make(chan int, 3)
+	go worker(10, c)
+	c <- 'a'
+	c <- 'b'
+	c <- 'c'
+	time.Sleep(time.Millisecond)
+}
+
 func chanDemo() {
-	var channels [10]chan int
+	var channels [10]chan<- int
 
 	// 要有一个 goroutine 接收数据
 	for i := 0; i < 10; i++ {
-		channels[i] = make(chan int)
-		go worker(i, channels[i])
+		channels[i] = createWorker(i)
 	}
 
 	for i := 0; i < 10; i++ {
@@ -26,6 +41,18 @@ func chanDemo() {
 	time.Sleep(time.Millisecond)
 }
 
+func channelClose() {
+	c := make(chan int, 3)
+	go worker(11, c)
+	c <- 'a'
+	c <- 'b'
+	c <- 'c'
+	close(c)
+	time.Sleep(time.Millisecond)
+}
+
 func main() {
 	chanDemo()
+	bufferedChannel()
+	channelClose()
 }
