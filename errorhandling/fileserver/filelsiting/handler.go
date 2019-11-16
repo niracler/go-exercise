739fcc21d4b3966fd3318ed3de0110ab6eb43ae3
Niracler <1026037967@qq.com>
2019-11-16 10:05:52 +0800

    web error handler

---
+++ errorhandling/fileserver/filelsiting/handler.go
@@ -0,0 +1,24 @@
+package filelsiting
+
+import (
+	"io/ioutil"
+	"net/http"
+	"os"
+)
+
+func HandleFileList(writer http.ResponseWriter, request *http.Request) error {
+	path := request.URL.Path
+	file, err := os.Open(path)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	all, err := ioutil.ReadAll(file)
+	if err != nil {
+		return err
+	}
+
+	_, _ = writer.Write(all)
+	return nil
+}
---
+++ errorhandling/fileserver/web.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"github.com/gpmgo/gopm/modules/log"
+	"go-exercise/errorhandling/fileserver/filelsiting"
+	"net/http"
+	"os"
+)
+
+type appHandler func(writer http.ResponseWriter, request *http.Request) error
+
+func errWrapper(handler appHandler) func(http.ResponseWriter, *http.Request) {
+	return func(writer http.ResponseWriter, request *http.Request) {
+		err := handler(writer, request)
+		if err != nil {
+			log.Warn("Error handling request: %s", err.Error())
+			code := http.StatusOK
+			switch {
+			case os.IsNotExist(err):
+				code = http.StatusNotFound
+			default:
+				code = http.StatusInternalServerError
+			}
+			http.Error(writer, http.StatusText(code), code)
+		}
+	}
+}
+
+func main() {
+	http.HandleFunc("/list/", errWrapper(filelsiting.HandleFileList))
+
+	err := http.ListenAndServe(":3334", nil)
+	if err != nil {
+		panic(err)
+	}
+}
