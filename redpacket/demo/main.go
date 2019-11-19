8af999d52afb8c9fdd11a13c92da69ca76de354c
Niracler <1026037967@qq.com>
2019-11-19 09:23:26 +0800

    red packet demo

---
+++ redpacket/demo/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+//二倍均值算法
+func DoubleAverage(count int64, amount int64) int64 {
+	const min = 1
+
+	if count == 1 {
+		return amount
+	}
+
+	max := amount - min*count //最大可用金额
+	avg := max / count        //最大可用平均值
+	avg2 := 2*avg + min
+
+	rand.Seed(time.Now().UnixNano())
+	x := rand.Int63n(avg2) + min
+
+	return x
+}
+
+func main() {
+	count, amount := int64(100), int64(100)
+	remain := amount
+	sum := int64(0)
+
+	for i := int64(0); i < count; i++ {
+		x := DoubleAverage(count-i, remain)
+		remain -= x
+		sum += x
+		fmt.Println(i+1, "=", float64(x)/float64(100))
+	}
+	fmt.Println("总和", float64(sum)/float64(100))
+}
