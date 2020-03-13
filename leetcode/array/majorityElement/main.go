0b9ec5728238478616709b717d8e6ef800cdfebe
Niracler <1026037967@qq.com>
2020-03-13 18:26:04 +0800

    majority element

---
+++ leetcode/array/majorityElement/main.go
@@ -0,0 +1,41 @@
+package main
+
+import "fmt"
+
+/*
+题目：给定一个大小为 n 的数组，找到其中的多数元素。多数元素是指在数组中出现次数大于 ⌊ n/2 ⌋ 的元素。
+
+你可以假设数组是非空的，并且给定的数组总是存在多数元素。
+
+示例 1:
+
+输入: [3,2,3]
+输出: 3
+示例 2:
+
+输入: [2,2,1,1,1,2,2]
+输出: 2
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/majority-element
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+*/
+
+func majorityElement(nums []int) int {
+
+	m := make(map[int]int)
+
+	for _, num := range nums {
+		m[num]++
+		if m[num] == len(nums) / 2 + 1 {
+			return num
+		}
+	}
+	return -1
+}
+
+func main() {
+	fmt.Println(majorityElement([]int{2,2,1,1,1,2,2}))
+}
+
+// 总结：
